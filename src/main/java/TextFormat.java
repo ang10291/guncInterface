9ea1732000bfb6dd98b406592d02c6aa1c62d7cc
ang10291 <krylovaangel@mail.ru>
2022-11-15 14:14:47 +0300

    divided the formation of the methods itself

--- src/main/java/TextFormat.java
+++ src/main/java/TextFormat.java
@@ -3,12 +3,18 @@ import javax.print.attribute.TextSyntax;
 public class TextFormat {
 	public String bold(String text, SyntaxFormat formatSyntax){
 		if (formatSyntax == SyntaxFormat.HTML){
-			return "<strong>" + text + "</strong>";
+			return boldHTML(text);
 		} else if (formatSyntax == SyntaxFormat.MD) {
-			return "**" + text + "**";
+			return boldMD(text);
 		}
 		throw  new IllegalArgumentException("There is no SyntaxType ={" + formatSyntax + "}");
 
+	}
+	private String boldMD(String text){
+		return "**" + text + "**";
+	}
+	private String boldHTML(String text){
+		return "<strong>" + text + "</strong>";
 	}
 	public enum SyntaxFormat {
 		MD, HTML
